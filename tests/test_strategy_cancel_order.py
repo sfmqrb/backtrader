7ba952a942fbb5e36b25fb482c7c3ae4e969c7a5
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-08-29 15:50:22 +0330

    feat: cancel works for stop stoplimit limit not market

--- tests/test_strategy_cancel_order.py
+++ tests/test_strategy_cancel_order.py
@@ -3,31 +3,58 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import backtrader as bt
+import testcommon
 
 
 class MyStrategy(bt.Strategy):
+    params = (
+        ("fast_period", 10),
+        ("slow_period", 50),
+    )
+
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
-            # Order has been submitted or accepted, attempt to cancel it
             self.cancel(order)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            # Order has been canceled, margin call, or rejected
-            pass
-
+        elif order.status in [order.Canceled]:
+            print("cancelled order")
+        elif order.status in [order.Margin]:
+            print("margin order")
+        elif order.status in [order.Rejected]:
+            print("rejected order")
         elif order.status == order.Completed:
             # Order has been completed
             pass
 
+    def __init__(self):
+        self.fast_ma = bt.indicators.SimpleMovingAverage(
+            self.data.close, period=self.params.fast_period
+        )
+        self.slow_ma = bt.indicators.SimpleMovingAverage(
+            self.data.close, period=self.params.slow_period
+        )
+
     def next(self):
-        # Your strategy logic here
-        pass
+        if not self.position:
+            buy_price = 5000
+            self.buy(exectype=bt.Order.Stop, price=buy_price)
+        elif self.fast_ma < self.slow_ma and self.position:
+            self.sell()
 
 
-cerebro = bt.Cerebro()
-cerebro.addstrategy(MyStrategy)
+chkdatas = 1
 
-# Add data feed and configure cerebro...
 
-# Run the backtest
-cerebro.run()
+def test_run(main=False):
+    datas = [testcommon.getdata(i) for i in range(chkdatas)]
+    testcommon.runtest(
+        datas,
+        MyStrategy,
+        runonce=True,
+        preload=True,
+        exbar=False,
+        plot=main,
+    )
+
+
+if __name__ == "__main__":
+    test_run(main=True)
